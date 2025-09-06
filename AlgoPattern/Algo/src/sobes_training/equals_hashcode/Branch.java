fedd6fe2ce16b7da7a0c1c1590623cbb6b634d04
Ren <rendg68@gmail.com>
2025-09-06 14:03:35 +0300

    Отредачил класс Branch.java

--- AlgoPattern/Algo/src/sobes_training/equals_hashcode/Branch.java
+++ AlgoPattern/Algo/src/sobes_training/equals_hashcode/Branch.java
@@ -1,12 +1,7 @@
 package sobes_training.equals_hashcode;
 
-//@Getter
-public class Branch {
-    private String name;
-    private String address;
+import lombok.Getter;
 
-    public Branch(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
+@Getter
+public record Branch(String name, String address) {
 }
